a2504d3dc5a4cea178b605e1d7248e1586057ea5
César <83521963+CesarBocel222@users.noreply.github.com>
2026-01-30 20:18:24 -0600

    Nombre

--- api/saludo.js
+++ api/saludo.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-  const nombre = req.query.nombre || "César Bocel 1094921";
+  const nombre = req.query.nombre || "anónimo";
 
   res.status(200).json({
     mensaje: `Hola ${nombre}`,
