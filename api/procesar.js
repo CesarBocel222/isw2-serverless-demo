4308651413eb6f0d2ab5eda9971d920e1a97ca84
César <83521963+CesarBocel222@users.noreply.github.com>
2026-02-09 18:15:46 -0600

    •	Agrega pruebas automatizadas y CI

--- api/procesar.js
+++ api/procesar.js
@@ -1,19 +1,7 @@
 export default function handler(req, res) {
-  // Obtenemos el nombre (query param)
   const nombre = req.query.nombre || "anónimo";
 
-  // --- RETO PARTE 1: Simular error ---
-  if (nombre === "error") {
-    return res.status(500).json({ 
-      error: "Simulación de fallo activada: El servicio de procesamiento falló." 
-    });
-  }
-
-  // --- RETO PARTE 2: Agregar Timestamp ---
-  const timestamp = new Date().toISOString();
-
   res.status(200).json({
-    resultado: `Nombre procesado: ${nombre.toUpperCase()}`,
-    timestamp: timestamp
+    resultado: `Nombre procesado: ${nombre.toUpperCase()}`
   });
 }
